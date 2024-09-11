0b43d8593ae31ecec2f63a66d1fc29830be21433
shadowkok <khaledkokash963@gmail.com>
2024-09-11 16:55:05 +0300

    LastNews sectione

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,17 @@ import { Hero } from '../components/Hero'
 import Features from '../components/Features'
 import { OurServices } from '../components/OurServices'
 import Team from '../components/Team'
-
+import About from '../components/About'
+import LastNews from '../components/LastNews'
 export const Home = () => {
   return (
     <div>
         <Hero/>
         <Features/>
+        
         <OurServices/>
+        <LastNews/>
         <Team/>
-        <OurServices/>
     </div>
   )
 }
