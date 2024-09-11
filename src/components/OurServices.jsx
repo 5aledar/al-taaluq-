e695ad1747d43f9d6f32cd9ee3865b156c550816
Malkalteer <alteermalk@gmail.com>
2024-09-11 15:57:18 +0300

    b

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 export const OurServices = () => {
   const features = [
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+
 import { Hero } from '../components/Hero'
 import Features from '../components/Features'
 import { OurServices } from '../components/OurServices'
 import Team from '../components/Team'
-import About from '../components/About'
 
 export const Home = () => {
   return (
     <div>
         <Hero/>
         <Features/>
+        <OurServices/>
+        <Team/>
     </div>
   )
 }
