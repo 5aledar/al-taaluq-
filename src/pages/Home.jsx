e345eadb44bcec475939a7871bc10e71f1fc00d0
Malkalteer <alteermalk@gmail.com>
2024-09-11 15:52:02 +0300

    malk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Hero } from '../components/Hero'
 import Features from '../components/Features'
+import About from '../components/About'
 
 export const Home = () => {
   return (
     <div>
         <Hero/>
         <Features/>
+        <About/>
     </div>
   )
 }
