d46c96c76f007868a9c9f0813fae193dd6609f9f
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-09-11 11:47:15 +0200

    -responsiv hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,16 +16,17 @@ export const Hero = () => {
           backgroundImage: showModal
             ? "url('/carousel-2.jpg')"
             : "url('/carousel-1.jpg')",
+            backgroundSize: "cover"
         }}
       >
         <div className={`absolute inset-0   bg-black bg-opacity-50 flex items-center`}>
           <div className={`container mx-auto`}>
-            <div className={`flex justify-end  absolute ${showModal ? "top-48 -left-64 " : "top-48 right-28"}`}>
+            <div className={`flex justify-end  absolute ${showModal ? " left-4 top-48 sm:-left-64 " : "top-48 right-4 sm:top-48 sm:right-28"}`}>
               <div className={`${showModal ?"text-left":"text-right"} lg:w-7/12`}>
-                <p className={`text-2xl text-white   ${showModal ? "animate__animated animate__slideInRight" : "animate__animated animate__slideInLeft"} `}>
+                <p className={`text-lg sm:text-2xl text-white   ${showModal ? "animate__animated animate__slideInRight" : "animate__animated animate__slideInLeft"} `}>
                   Welcome to <strong className="text-orange-500 ">GrowMark</strong>
                 </p>
-                <h1 className={`text-6xl font-extrabold text-white mb-5 ${showModal ? "animate__animated animate__slideInRight" : "animate__animated animate__slideInLeft"} `}>
+                <h1 className={`text-2xl sm:text-6xl font-extrabold text-white mb-5 ${showModal ? "animate__animated animate__slideInRight" : "animate__animated animate__slideInLeft"} `}>
                   {showModal ? (
                     <strong className="transition-all">Ready to Grow Your Business</strong>
                   ) : (
@@ -45,11 +46,11 @@ export const Hero = () => {
       </div>
       <button
         onClick={handelClick}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-orange-500 bg-opacity-75 hover:bg-orange-600 text-gray-800 rounded-full p-3 shadow-lg transition duration-300"
+        className="absolute   top-1/2 left-4 transform -translate-y-1/2 bg-orange-500 bg-opacity-75 hover:bg-orange-600 text-gray-800 rounded-full p-3 shadow-lg transition ease-in-out delay-150  hover:scale-110 duration-300 "
         type="button"
       >
         <svg
-          className="w-6 h-6"
+          className="w-6 h-6 "
           fill="currentColor"
           viewBox="0 0 16 16"
           xmlns="http://www.w3.org/2000/svg"
@@ -64,7 +65,7 @@ export const Hero = () => {
       </button>
       <button
         onClick={handelClick}
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-orange-500 bg-opacity-75 hover:bg-orange-600 text-gray-800 rounded-full p-3 shadow-lg transition duration-300"
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-orange-500 bg-opacity-75 hover:bg-orange-600 text-gray-800 rounded-full p-3 shadow-lg  transition ease-in-out delay-150  hover:scale-110 duration-300"
         type="button"
       >
         <svg
