eb48162542cbb7c244ad338921965633c27de293
shadowkok <khaledkokash963@gmail.com>
2024-09-11 17:12:47 +0300

    n

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
-import About from '../components/About'
+
 import { Hero } from '../components/Hero'
 import Features from '../components/Features'
 import { OurServices } from '../components/OurServices'
 import Team from '../components/Team'
 import TestomonialContainer from '../containers/TestomonialContainer'
-import { OurActivities } from '../components/OurActivities'
 
-// import About from '../components/About'
+import About from '../components/About'
 import LastNews from '../components/LastNews'
 export const Home = () => {
   return (
@@ -18,7 +17,6 @@ export const Home = () => {
         <LastNews/>
         <Team/>
         <TestomonialContainer />
-        <OurServices/>
     </div>
   )
 }
