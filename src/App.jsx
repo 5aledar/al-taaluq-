a3e08910033d100c35302475d281e7fa2c0a7984
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-09-11 10:21:27 +0200

    -add pages folder and Route Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Routes , Route } from 'react-router-dom'
 import './App.css'
-
+import { Home } from './pages/Home'
 function App() {
 
 
   return (
     <Routes>
       {/* <Route /> */}
+      <Route path='/' element={<Home />} />
     </Routes>
   )
 }
