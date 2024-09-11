e7497687e349b66599c86dd4b3c645936eea36a0
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-09-11 18:34:04 +0200

    -fixed some aissues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-
+import About from '../components/About'
 import { Hero } from '../components/Hero'
 import Features from '../components/Features'
 import { OurServices } from '../components/OurServices'
 import Team from '../components/Team'
 import TestomonialContainer from '../containers/TestomonialContainer'
-
 import LastNews from '../components/LastNews'
 export const Home = () => {
   return (
