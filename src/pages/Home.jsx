42009f017b58cd6de53585a9b802178da6e03aff
shadowkok <khaledkokash963@gmail.com>
2024-09-11 15:23:14 +0300

    Our team sectione

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Hero } from '../components/Hero'
 import Features from '../components/Features'
+import Team from '../components/Team'
 
 export const Home = () => {
   return (
     <div>
         <Hero/>
         <Features/>
+        <Team/>
     </div>
   )
 }
