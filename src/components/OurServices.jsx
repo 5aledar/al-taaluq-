2be32f36fa6107c579d724a59478a984e643a64f
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-09-11 14:17:31 +0200

    -our servisus

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -22,15 +22,35 @@ export const OurServices = () => {
       title: "24/7 Support",
       description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam",
     },
+    {
+      imgSrc: "/icon-5.png",
+      title: "24/7 Support",
+      description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam",
+    },
+    {
+      imgSrc: "/icon-6.png",
+      title: "24/7 Support",
+      description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam",
+    },
+    {
+      imgSrc: "/icon-7.png",
+      title: "24/7 Support",
+      description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam",
+    },
+    {
+      imgSrc: "/icon-8.png",
+      title: "24/7 Support",
+      description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam",
+    },
   ];
 
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto py-10 ">
       <div
-        className="text-center mx-auto max-w-lg animate__animated animate__fadeInUp "
+        className="text-center mx-auto max-w-lg animate__animated animate__fadeInUp mb-14 "
         style={{
           animationDelay: "0.1s",
-        }} 
+        }}
       >
         <p className="text-lg font-medium text-blue-600">Our Services</p>
         <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-5">
@@ -40,18 +60,24 @@ export const OurServices = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-center">
         {features.map((feature, index) => (
           <div
-            className="bg-white p-6 border rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+            className=" relative bg-white p-6 border    transition ease-in-out   hover:-translate-y-5  hover:bg-stone-100 duration-300 group"
             key={index}
           >
-            <div className="mb-4 flex justify-center text-orange-500">
+            <div className="mb-4 flex justify-center ">
               <img
-                className="w-20 h-20"
+                className="w-20 h-20  "
                 src={feature.imgSrc}
                 alt={feature.title}
               />
             </div>
             <h5 className="text-xl font-semibold mb-2">{feature.title}</h5>
-            <p className="text-gray-600">{feature.description}</p>
+            <p className="text-slate-600 text-lg">{feature.description}</p>
+
+            <div className=" cursor-pointer absolute -bottom-6 right-0   transition-all ease-in-out duration-500  bg-orange-300 text-white w-full opacity-0 group-hover:opacity-100 hover:bg-orange-500 ">
+              <p className="font-bold items-center m-auto leading-10   ">
+                Read More<span className="ml-1 text-lg font-bold">&raquo;</span>
+              </p>
+            </div>
           </div>
         ))}
       </div>
