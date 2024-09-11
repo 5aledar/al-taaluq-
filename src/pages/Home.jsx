4fdf730596c12b1dd799aeaaba0a3ef6887c416a
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-09-11 09:50:36 -0400

    testmonials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,16 @@ import { Hero } from '../components/Hero'
 import Features from '../components/Features'
 import { OurServices } from '../components/OurServices'
 import Team from '../components/Team'
+import TestomonialContainer from '../containers/TestomonialContainer'
 
 export const Home = () => {
   return (
     <div>
         <Hero/>
         <Features/>
+        <OurServices />
         <Team/>
+        <TestomonialContainer />
     </div>
   )
 }
